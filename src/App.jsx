749480ac96d56b0cf28e71880e9c0c1cf9868357
Jonathan <jonathan.d.depaul@gmail.com>
2024-02-10 09:05:45 -0500

    Output state data

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import { CORE_CONCEPTS } from "./assets/data.js";
 import Header from "./components/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./assets/data.js";
 
 function App() {
-  const [selectedTopic, setTopic] = useState("Please click a button");
+  const [selectedTopic, setTopic] = useState();
   //let tabContent = "Please click a button"
 
   function clickHandler(selectedButton) {
@@ -37,7 +38,19 @@ function App() {
             <TabButton onSelect={() => clickHandler("props")}>Props</TabButton>
             <TabButton onSelect={() => clickHandler("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id='tab-content'>
+            {!selectedTopic ? (
+              <p>Please select a topic.</p>
+            ) : (
+              <>
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                  <code>{EXAMPLES[selectedTopic].code}</code>
+                </pre>
+              </>
+            )}
+          </div>
         </section>
       </main>
     </div>
