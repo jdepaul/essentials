30c121208cebd4b04b44c68b7e81bbfc9ed8ea21
Jonathan <jonathan.d.depaul@gmail.com>
2024-02-13 20:30:15 -0500

    added styling and map

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,24 @@ import { EXAMPLES } from "./assets/data.js";
 
 function App() {
   const [selectedTopic, setTopic] = useState();
-  //let tabContent = "Please click a button"
+
+  let tabContent = "Please select a topic";
 
   function clickHandler(selectedButton) {
     //selected button => components, jsx, props, state
     setTopic(selectedButton);
+  }
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id='tab-content'>
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -22,35 +35,20 @@ function App() {
         <section id='core-concepts'>
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((concept) => <CoreConcept key={concept.title} {...concept} /> )}
           </ul>
         </section>
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => clickHandler("components")}>
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => clickHandler("components")}>
               Components
             </TabButton>
-            <TabButton onSelect={() => clickHandler("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => clickHandler("props")}>Props</TabButton>
-            <TabButton onSelect={() => clickHandler("state")}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'}  onSelect={() => clickHandler("jsx")}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'}  onSelect={() => clickHandler("props")}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'}  onSelect={() => clickHandler("state")}>State</TabButton>
           </menu>
-          <div id='tab-content'>
-            {!selectedTopic ? (
-              <p>Please select a topic.</p>
-            ) : (
-              <>
-                <h3>{EXAMPLES[selectedTopic].title}</h3>
-                <p>{EXAMPLES[selectedTopic].description}</p>
-                <pre>
-                  <code>{EXAMPLES[selectedTopic].code}</code>
-                </pre>
-              </>
-            )}
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
